ae40670a3b79ad88804866bc0aae3e54939440a2
Tyler Stanish <tystanish@gmail.com>
2017-10-28 00:58:32 -0400

    finally got npm link to work with issues regarding native linking with expo and react-native-svg

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	View
+} from 'react-native';
+import {VictoryBar} from 'victory-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class extends React.Component{
+	render(){
+		return(
+			<View>
+				<VictoryBar/>
+			</View>
+		)
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
